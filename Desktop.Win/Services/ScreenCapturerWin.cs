5feda1693c7b0423b4ea39a191b789137c578865
robinc80 <80622028+robinc80@users.noreply.github.com>
2021-04-12 14:01:06 +0200

    update

--- Desktop.Win/Services/ScreenCapturerWin.cs
+++ Desktop.Win/Services/ScreenCapturerWin.cs
@@ -83,6 +83,8 @@ namespace Remotely.Desktop.Win.Services
             {
                 try
                 {
+					 Win32Interop.SwitchToInputDesktop();
+					 
                     if (NeedsInit)
                     {
                         Logger.Write("Init needed in GetNextFrame.");
