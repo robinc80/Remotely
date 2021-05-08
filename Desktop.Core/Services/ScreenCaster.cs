7279d6b0e70a2f4c48a2a344c5afd2272a65463a
robinc80 <80622028+robinc80@users.noreply.github.com>
2021-05-08 10:17:00 +0200

    Add orientation to DirectXOutput model to detect orientation of non-p…

--- Desktop.Core/Services/ScreenCaster.cs
+++ Desktop.Core/Services/ScreenCaster.cs
@@ -40,7 +40,6 @@ namespace Remotely.Desktop.Core.Services
 
         public async Task BeginScreenCasting(ScreenCastRequest screenCastRequest)
         {
-        
             try
             {
                 var sendFramesLock = new SemaphoreSlim(1, 1);
@@ -49,6 +48,7 @@ namespace Remotely.Desktop.Core.Services
                 var currentQuality = _maxQuality;
                 Bitmap currentFrame = null;
                 Bitmap previousFrame = null;
+                var sw = Stopwatch.StartNew();
 
                 var viewer = ServiceContainer.Instance.GetRequiredService<Viewer>();
                 viewer.Name = screenCastRequest.RequesterName;
@@ -118,6 +118,9 @@ namespace Remotely.Desktop.Core.Services
                 {
                     try
                     {
+                        TaskHelper.DelayUntil(() => sw.Elapsed.TotalMilliseconds > 40, TimeSpan.FromSeconds(5));
+                        sw.Restart();
+
                         if (viewer.IsUsingWebRtcVideo)
                         {
                             Thread.Sleep(100);
@@ -187,8 +190,8 @@ namespace Remotely.Desktop.Core.Services
                                     Debug.WriteLine($"Quality Reduced: {currentQuality}");
                                 }
                             }
-                            
-							using var clone = currentFrame.Clone(diffArea, currentFrame.PixelFormat);
+
+                            using var clone = currentFrame.Clone(diffArea, currentFrame.PixelFormat);
                             //var resizeW = diffArea.Width * currentQuality / _maxQuality;
                             //var resizeH = diffArea.Height * currentQuality / _maxQuality;
                             //using var resized = new Bitmap(clone, new Size(resizeW, resizeH));
