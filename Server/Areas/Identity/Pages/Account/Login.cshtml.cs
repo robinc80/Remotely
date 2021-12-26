a3212c33fcbcff3837e5a68dcc0db98908180740
Robin <r.collet@aol.co.uk>
2021-12-26 10:27:53 +0100

    Francisation

--- Server/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Server/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -91,7 +91,7 @@ namespace Remotely.Server.Areas.Identity.Pages.Account
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User logged in.");
+                    _logger.LogInformation("Utilisateur connecté.");
                     return LocalRedirect(returnUrl);
                 }
                 if (result.RequiresTwoFactor)
--- Server/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Server/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -118,8 +118,8 @@ namespace Remotely.Server.Areas.Identity.Pages.Account
                         values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
                         protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    await _emailSender.SendEmailAsync(Input.Email, "Confirmer votre adresse mail",
+                        $"Veuillez confirmer votre compte en <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>cliquant ici</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
